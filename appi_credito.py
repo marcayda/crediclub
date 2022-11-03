8b6860144498eee1d1c43f4c6eedf5e58f1d159b
marcayda <tm_guerrerov@hotmail.com>
2022-11-02 21:10:42 -0600

    primer commit

---
+++ appi_credito.py
@@ -0,0 +1,19 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def query_example():
+    # if key doesn't exist, returns None
+    language = request.args.get('language')
+
+    # if key doesn't exist, returns a 400, bad request error
+    framework = request.args['framework']
+
+    # if key doesn't exist, returns None
+    website = request.args.get('website')
+
+    return '''
+              <h1>The language value is: {}</h1>
+              <h1>The framework value is: {}</h1>
+              <h1>The website value is: {}'''.format(language, framework, website)
